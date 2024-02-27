c8d4f1dc88d507a149a37973d0c4101e1985456c
Kevin Cosme <official.kevo12@gmail.Cosme>
2024-02-27 12:58:33 -0600

    Used the rewind method which caused the deletion of the functions and moved the method to an initialize function that would make it easier to affect all 4 types. As well as made a new klass function that would implement all 4 in that intitialize function

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,31 +1,100 @@
 class GildedRose
-  attr_reader :name, :days_remaining, :quality
+  attr_reader :name, :item
 
-  def initialize(name:, days_remaining:, quality:)
-    @name = name
-    @days_remaining = days_remaining
-    @quality = quality
+  def initialize(name, quality, days_remaining)
+    @item = klass_for(name).new(quality, days_remaining)
+  end
+
+  def klass_for(name)
+    case name
+    when 'normal'
+      Normal
+    when 'Aged Brie'
+      Brie
+    when 'Sulfuras, Hand of Rangnaros'
+      Sulfuras
+    when 'Backstage passes to a TAFKAL80ETC'
+      Backstage
+    end
+  end
+
+
+  '''def normal_tick
+    @item = Normal.new(quality, days_remaining)
+    item.tick
+  end
+
+  def brie_tick
+    @item = Brie.new(quality, days_remaining)
+    item.tick
+  end
+
+  def sulfuras_tick
+    @item = Sulfuras.new(quality, days_remaining)
+    item.tick
+  end
+
+  def backstage_tick
+    @item = Backstage.new(quality, days_remaining)
+    item.tick
+  end
+'''
+  def quality
+    return item.quality if item
+    @quality
+  end
+  def days_remaining
+    return item.days_remaining if item
+    @days_remaining
+  end
+
+class Normal
+  attr_reader :quality :days_remaining
+  def initialize(:quality, :days_remaining)
+    @quality, @days_remaining = quality, days_remaining
   end
 
   def tick
+    @days_remaining -= 1
+    return if @quality == 0
 
-    if name = 'normal'
-      return normal_tick
-    end
+    @quality -= 1
+    @quality -=1 if @days_remaining <= 0
+  end
+end
 
 
-    case name
-    when 'normal'
-      return normal_tick
-    when 'Aged Brie'
-      return brie_tick
-    when 'Sulfuras, Hand of Ragnaros'
-      return sulfuras_tick
-    when "Backstage passes to a TAFKAL80ETC concert"
-      return backstage_tick
-    end
+class Brie
+  attr_reader :quality :days_remaining
+  def initialize(:quality, :days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
 
-  def backstage_tick
+  def tick
+    @days_remaining -= 1
+    return if @quality >= 50
+    @quality += 1
+    @quality += 1 if @days_remaining <= 0
+  end
+end
+
+
+class Sulfuras
+  attr_reader :quality :days_remaining
+  def initialize(:quality, :days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+  def tick
+  end
+end
+
+
+class Backstage
+  attr_reader :quality :days_remaining
+  def initialize(:quality, :days_remaining)
+    @quality, @days_remaining = quality, days_remaining
+  end
+  def tick
     @days_remaining -= 1
     return if @quality = 50
     return @quality = 0 if @days_remaining < 0
@@ -33,25 +102,4 @@ class GildedRose
     @quality += 1 if @days_remaining < 10
     @quality += 1 if @days_remaining < 5
   end
-
-
-  def sulfuras_tick
-
-  end
-  def brie_tick
-    @days_remaining -= 1
-    return if @qualify >= 50
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0
-  end
-
-
-  def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -=1 if @days_remaining <= 0
-  end
-
 end
